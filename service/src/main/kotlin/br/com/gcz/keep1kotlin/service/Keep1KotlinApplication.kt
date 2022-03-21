93cd19d6c7d842f1d222a048fcbb672ae58e7b14
gudeck <guzuc@outlook.com>
2022-03-21 12:03:29 -0300

    initial commit

---
+++ service/src/main/kotlin/br/com/gcz/keep1kotlin/service/Keep1KotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.gcz.keep1kotlin.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Keep1KotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<Keep1KotlinApplication>(*args)
+}
---
+++ service/src/test/kotlin/br/com/gcz/keep1kotlin/service/Keep1KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.gcz.keep1kotlin.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Keep1KotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
